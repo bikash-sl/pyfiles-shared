aa4430a7d71fb3f98fe28422e082d803447b6ca2
bikash-sl <bikashsahu2018-sl@yahoo.com>
2020-07-25 17:39:37 +0530

    First commit

---
+++ Exercise_P1/Ex-5.py
@@ -0,0 +1,11 @@
+"""
+5. Write a Python program which accepts the user's first and last name 
+and print them in reverse order with a space between them.
+"""
+
+first = input("Enter the first name: ")
+last = input("Enter the last name: ")
+
+print(f"Your name in reverse is: {last} {first}")
+
+input("\nPress Enter to exit ... ... ... ")
