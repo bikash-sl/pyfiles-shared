25128fb0d5f730ff27dcea0036ffd177261810a2
bikash-sl <bikashsahu2018-sl@yahoo.com>
2020-11-26 00:39:29 +0530

    first commit

---
+++ Python_Beginner_One/Ch_1/Q3- External module.py
@@ -0,0 +1,8 @@
+"""
+Install an external module and use it to perform an operation of your interest.
+
+Soln: Used playsound module (pip install playsound)
+"""
+
+from playsound import playsound
+playsound('pyfiles-shared/Python_Beginner_One/Ch_1/bensound-littleidea.mp3')
---
+++ Python_Beginner_One/Ch_1/Q4- OS module.py
@@ -0,0 +1,6 @@
+"""
+Print the contents of a directory using OS module.
+"""
+
+import os
+print(os.listdir())
